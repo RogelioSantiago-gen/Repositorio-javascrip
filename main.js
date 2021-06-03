096966a49557661e0563bced89efb1ef7ef111c9
Rogelio <rogelio.martinezsantiago1@gmail.com>
2021-06-03 11:29:03 -0700

    main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+/*
+function persona(nombre,apellidos,edad) {
+
+    this.nombre = nombre;
+
+    this.apellidos = apellidos;
+   
+    this.edad = edad;
+
+
+}
+
+var objetopersona = new persona("Rogelio", "martinez santiago",24);
+
+console.log(objetopersona.nombre);
+
+var persona2 = {
+    nombre: "Rogelio",
+    apellidos: "Martinez Santiago",
+    edad: 24,
+    saludar: function() {
+        console.log("Hola soy " + this.nombre + " " + this.apellidos)
+    }
+}
+
+persona2.saludar();
+*/
+
+
